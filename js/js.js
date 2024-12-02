91d1904f48e7bd31d5b0c2c243715ddf558c2fcf
Andrey <120495791+Giramon@users.noreply.github.com>
2024-12-02 18:21:13 +0500

    Update js.js

--- js/js.js
+++ js/js.js
@@ -20,3 +20,39 @@ window.addEventListener("click", function (e) {
         modal.classList.remove("registration_menu--open");
     }
 });
+
+function myFunction() {
+    document.getElementById("myDropdown").classList.toggle("show");
+}
+
+function myFunction2() {
+    document.getElementById("myDropdown2").classList.toggle("show2");
+}
+
+window.onclick = function(event) {
+  if (!event.target.matches('.dropmenu2')) {
+
+    var dropdowns = document.getElementsByClassName("select_city_hidden2");
+    var i;
+    for (i = 0; i < dropdowns.length; i++) {
+      var openDropdown = dropdowns[i];
+      if (openDropdown.classList.contains('show2')) {
+        openDropdown.classList.remove('show2');
+      }
+    }
+  }
+}
+
+window.onclick = function(event) {
+    if (!event.target.matches('.dropmenu')) {
+  
+      var dropdowns = document.getElementsByClassName("select_city_hidden");
+      var i;
+      for (i = 0; i < dropdowns.length; i++) {
+        var openDropdown = dropdowns[i];
+        if (openDropdown.classList.contains('show')) {
+          openDropdown.classList.remove('show');
+        }
+      }
+    }
+  }
